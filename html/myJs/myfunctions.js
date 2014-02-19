11e0d6ae8e11eddc6456c1e8fc2bf4e7a0bbec05
Magnus Lubeck <magnus.lubeck@gmail.com>
2014-02-19 10:00:44 +0100

    added gauge for one thermometer

--- html/myJs/myfunctions.js
+++ html/myJs/myfunctions.js
@@ -99,6 +99,109 @@ function getSpinnerOptsSmall(){
   // end of function draw_chart
 }
 
+function printGauge(myPane, myData){
+  console.log("XXX printing gauge");
+
+    var chart = new Highcharts.Chart({
+    
+        chart: {
+            renderTo: myPane,
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        
+        title: {
+            text: 'Thermometer'
+        },
+        
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#333'],
+                        [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+           
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 200,
+            
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+    
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: myData.aliases[0]
+            },
+            plotBands: [{
+                from: 0,
+                to: 120,
+                color: '#55BF3B' // green
+            }, {
+                from: 120,
+                to: 160,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 160,
+                to: 200,
+                color: '#DF5353' // red
+            }]        
+        },
+    
+        series: [{
+            name: myData.sensorName ,
+            data: [ +(myData.temperature) ] ,
+            tooltip: {
+                valueSuffix: ' km/h'
+            }
+        }]
+    
+    });
+
+  return 0;
+}
+
+
 //----------------------------------------------------------
 // updateDevicesPane
 //----------------------------------------------------------
