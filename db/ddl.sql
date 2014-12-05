f229217cbb70552fefa1547a0934c16da465dd47
Magnus Lubeck <magnus.lubeck@gmail.com>
2014-12-05 11:00:01 +0000

    db ddl and config

---
+++ db/ddl.sql
@@ -0,0 +1,4 @@
+create table device (id text, type text, path text);
+create table alias (id text, alias text);
+create table devicegroup (groupname text, device_id text);
+CREATE TABLE plotgroup( groupname TEXT, device_id TEXT, plot_type TEXT);
