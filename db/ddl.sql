1c12701adc839556c3d64852dd354556f8faca35
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-01-13 09:22:54 +0000

    updated ddl for plotconfig

--- db/ddl.sql
+++ db/ddl.sql
@@ -2,5 +2,5 @@ create table device (id text, type text, path text);
 create table alias (id text, alias text);
 create table devicegroup (groupname text, device_id text);
 create table plotgroup ( groupname TEXT, device_id TEXT, plot_type TEXT);
-create table plotconfig (name text, plotgroup text, timespan text, size integer, prio integer);
+CREATE TABLE plotconfig (name text, plotgroup text, timespan text, size integer, prio integer, visible boolean);
 create table arne (name text);
