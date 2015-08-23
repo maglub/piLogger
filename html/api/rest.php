1368e1f65bc30ac0c43d3d442500b7e7c0964bf7
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-08-23 18:49:19 +0200

    new release with php Slim as framework for REST API

---
+++ html/api/rest.php
@@ -0,0 +1,76 @@
+<?php
+	#--- go to xyz/includes (which has composer.phar and composer.json)
+	#--- run ./composer.phar install
+	#
+	#--- the .htaccess file in the ./api directory can look like this:
+	#
+	#--- maglub@ubuntu-14:~/dev/web/public_html/api$ cat .htaccess 
+	#--- RewriteEngine on
+	#--- RewriteCond %{REQUEST_FILENAME} !-f
+	#--- RewriteRule ^ /api/rest.php [QSA,L]
+	
+	
+	#--- default Nordiska config
+	require_once($_SERVER['DOCUMENT_ROOT']."/stub.inc");
+	require_once($root."/vendor/autoload.php");
+
+    require_once($root . "myfunctions.inc");
+
+ 
+    //db_connect();
+	
+	#--- this is a workaround, so that json_encode in SlimJson works
+	#--- since our database is iso-latin-1
+	#--- we can remove this as soon as we have migrated our database
+	//mysql_set_charset("utf8");
+	
+	#--- instantiate Slim and SlimJson
+	$app = new \Slim\Slim();
+    $app->add(new \SlimJson\Middleware());
+	
+
+	#======== helper functions
+	function o2h($obj){ #--- object to hash helper function (since json_encode cannot serialize php objects)
+		$ret = Array();
+		foreach($obj as $key => &$field){
+			if(is_object($field)){
+				$field = o2h($field);
+			}
+			$ret[$key] = $field;
+		}
+		return $ret;
+	}
+
+	//$isAuthenticated = checkMySession(CONST_TRIAL);
+	$isAuthenticated = true;
+	#==================================
+	# MAIN
+	#==================================
+
+	$app->get('/sensor', function() use ($app) {
+		$res=getDevices();
+		$app->render(200,o2h($res));
+	}//end of function
+	);
+
+	$app->get('/sensor/all/info', function() use ($app, $root) {
+		$resOs = shell_exec($root . "/../bin/listDevices --json --info");
+		// the output is already in json format
+		print $resOs;
+//		$res=getDevices();
+//		$app->render(200,o2h($res));
+	}//end of function
+	);
+
+
+
+	$app->get('/sensor/:id', function($id) use ($app) {
+		$res=getSensorById($id);
+		$app->render(200,o2h($res));
+	}//end of function
+	);
+	
+	
+	$app->run();
+
+?>
