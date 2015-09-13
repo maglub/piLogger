617c952b4d1e5a8ced0c825658d8841b8f4da059
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-09-13 16:11:58 +0200

    Refactoring - moving shell calls from php into pure php

--- html/api/rest.php
+++ html/api/rest.php
@@ -9,12 +9,25 @@
 	#--- RewriteCond %{REQUEST_FILENAME} !-f
 	#--- RewriteRule ^ /api/rest.php [QSA,L]
 	
-	
-	#--- default Nordiska config
+	//set up environment for cli
+	if (!(isset($_SERVER['DOCUMENT_ROOT']) && $_SERVER['DOCUMENT_ROOT'] !== "")) {
+		$_SERVER['HTTP_HOST'] = "cron";
+		// add ".." to the directory name to point to "./html"
+		$_SERVER['DOCUMENT_ROOT'] = __DIR__ . "/..";
+		$argv = $GLOBALS['argv'];
+		array_shift($GLOBALS['argv']);
+		#$pathInfo = implode('/', $argv);
+		$pathInfo = $argv[0];
+	}
+
+
+	if(isset($ENV['debug'])) { print "apa\n"; };
+
 	require_once($_SERVER['DOCUMENT_ROOT']."/stub.inc");
 	require_once($root."/vendor/autoload.php");
 
-    require_once($root . "myfunctions.inc");
+	require_once($root . "myfunctions.inc");
+
 
  
     //db_connect();
@@ -26,6 +39,15 @@
 	
 	#--- instantiate Slim and SlimJson
 	$app = new \Slim\Slim();
+
+        //if run from the command-line
+	if ($_SERVER['HTTP_HOST'] === "cron"){
+		// Set up the environment so that Slim can route
+		$app->environment = Slim\Environment::mock([
+		    'PATH_INFO'   => $pathInfo
+		]);
+	}
+
     $app->add(new \SlimJson\Middleware());
 	
 
@@ -48,7 +70,7 @@
 	#==================================
 
 	$app->get('/sensor', function() use ($app) {
-		$res=getDevices();
+		$res=getSensors();
 		$app->render(200,o2h($res));
 	}//end of function
 	);
@@ -68,9 +90,8 @@
 	);
 	
 	$app->get('/sensor/:id/temperature', function($id) use ($app,$root) {
-		$resOs = shell_exec("${root}/../bin/getTemperature -rrd $id");
-		//print "{\"result\":\"ok\", \"command\":\"${root}/../bin/logTemperature $id $temperature\",\"message\":\"{$resOs}\"}";
-		print "{\"temperature\":${resOs}}";
+		$res =  Array( "id" => $id, "temperature" => getLastTemperatureBySensorId($id));
+		$app->render(200,o2h($res));
 	}//end of function
 	);
 	
@@ -82,12 +103,19 @@
 	);
 	
 	
-	$app->get('/sensor/all/info', function() use ($app, $root) {
+	$app->get('/sensor/all/oldInfo', function() use ($app, $root) {
 		$resOs = shell_exec($root . "/../bin/listDevices --json --info");
-		// the output is already in json format
 		print $resOs;
-//		$res=getDevices();
-//		$app->render(200,o2h($res));
+	}//end of function
+	);
+
+	$app->get('/sensor/all/info', function() use ($app, $root) {
+		$res = getSensorInfoAll();
+		#$resOs = shell_exec($root . "/../bin/listDevices --json --info");
+		// the output is already in json format
+		#print $resOs;
+//		$res=getSensors();
+		$app->render(200,o2h($res));
 	}//end of function
 	);
 
@@ -108,12 +136,6 @@
 		$app->render(200,o2h($res));
 	}//end of function
 	);
-	
-
-
-
-	
-	
 	
 	$app->run();
 
