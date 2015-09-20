6dcc271ca350426c2ec3443c5f69caff70751be0
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-09-20 13:51:44 +0200

    Renamed device related database DDL to sensor related DDL.

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,6 +1,6 @@
 create table alias (id text, alias text);
 create table sensor (id text, type text, path text);
 create table sensormetric (id text, metric text);
-create table sensorgroup (groupname text, device_id text);
-create table plotgroup ( groupname TEXT, device_id TEXT, plot_type TEXT);
+create table sensorgroup (groupname text, sensor_id text);
+create table plotgroup ( groupname TEXT, sensor_id TEXT, plot_type TEXT);
 CREATE TABLE plotconfig (name text, plotgroup text, timespan text, size integer, prio integer, visible boolean);
