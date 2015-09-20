39c0e695bf4eb63c38d1c56318e83b45c8bf4528
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-09-20 13:12:33 +0200

    updated database creation scripts using the new names

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,6 +1,6 @@
-create table device (id text, type text, path text);
-create table devicemetric (id text, metric text);
 create table alias (id text, alias text);
-create table devicegroup (groupname text, device_id text);
+create table sensor (id text, type text, path text);
+create table sensormetric (id text, metric text);
+create table sensorgroup (groupname text, device_id text);
 create table plotgroup ( groupname TEXT, device_id TEXT, plot_type TEXT);
 CREATE TABLE plotconfig (name text, plotgroup text, timespan text, size integer, prio integer, visible boolean);
