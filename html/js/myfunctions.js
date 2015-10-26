a2ea18b2fa01f84158090f475246f59a47c25209
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-10-26 14:16:35 +0100

    Copied code from piLogger-cloud to help with debugging

--- html/js/myfunctions.js
+++ html/js/myfunctions.js
@@ -52,8 +52,10 @@ function getSpinnerOptsSmall(){
 //----------------------------------------------------------
 // draw_chart(myData)
 //----------------------------------------------------------
-  function draw_chart(myPane, myData){
+  function draw_chart(myDiv, myData){
 
+
+/*
      var plotData = [];
      var plotDataSensors = [ ];
 
@@ -103,6 +105,68 @@ function getSpinnerOptsSmall(){
                 }
             });
 
+
+*/
+
+      // variables for holding information from myData
+      var plotData = [];
+      var plotDataSensors = [ ];
+      var plotDataArray = new Array();
+   
+      // loop over all json data that we get and store them in an array
+      for (i=0; i<myData.length ; i++) {
+         plotDataArray[i] = new Object();
+         plotDataArray[i].data = myData[i].temperature.slice(0);
+         plotDataArray[i].name = myData[i].sensor.slice(0);
+         plotData[i] = myData[i].temperature.slice(0);
+         plotDataSensors[i] = myData[i].sensor.slice(0);
+      }
+   
+      // tell Highcharts not to use UTC timezone
+      Highcharts.setOptions({
+         global: {
+            useUTC: false
+         }
+      });
+
+      // now generate the chart and display it
+      new Highcharts.Chart({
+
+         chart: {
+            type: 'line',
+            zoomType: 'x',
+            renderTo: myDiv
+         },
+         title: {
+            text: 'Temperatures: ' + myDiv
+         },
+         xAxis: {
+            type: 'datetime',
+            title: 'Date',
+            maxZoom: 1800000
+         },
+         yAxis: {
+            title: {
+               text: 'C'
+            }
+         },
+         credits: {
+            enabled: false
+         },
+         plotOptions: { 
+            line: {
+               animation: false 
+            },
+            series: {
+               animation: false ,
+               marker: { 
+                  enabled: false 
+               } 
+            }
+         },
+         series:  plotDataArray
+      });	  
+	  
   // end of function draw_chart
 }
 
