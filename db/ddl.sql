7ec3ebe7e65be4180e3aa47a316cccec1eaa528a
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-12-03 12:32:21 +0100

    Updated ddl.sql to reflect the latest changes in the database

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,7 +1,9 @@
-create table alias (id text, alias text);
-create table sensor (id text, type text, path text);
-create table sensormetric (id text, metric text);
-create table sensorgroup (groupname text, sensor_id text);
-create table plotgroup ( groupname TEXT, sensor_id TEXT, plot_type TEXT);
-CREATE TABLE plotconfig (name text, plotgroup text, timespan text, size integer, prio integer, visible boolean);
+CREATE TABLE alias (id text, alias text);
+CREATE TABLE sensorgroup (groupname text, sensor_id text);
+CREATE TABLE sensormetric (id text, metric text);
+CREATE TABLE plotgroup ( groupname TEXT, sensor_id TEXT, plot_type TEXT);
 CREATE TABLE version (id text);
+CREATE TABLE passwd(uid integer, username string, password string);
+CREATE TABLE "plotconfig"( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , visible boolean); CREATE TABLE plotconfig_new( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , after integer, before integer);
+CREATE TABLE "sensor"(id TEXT,type TEXT,active NUM, metric text, path text);
+
