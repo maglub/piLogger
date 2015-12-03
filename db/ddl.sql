6d1d4ed0a170190c463d92b32f7f22c447a241a5
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-12-03 12:34:45 +0100

    Updated ddl.sql to reflect the latest changes in the database, 2nd try

--- db/ddl.sql
+++ db/ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE sensorgroup (groupname text, sensor_id text);
 CREATE TABLE sensormetric (id text, metric text);
 CREATE TABLE plotgroup ( groupname TEXT, sensor_id TEXT, plot_type TEXT);
 CREATE TABLE version (id text);
-CREATE TABLE passwd(uid integer, username string, password string);
-CREATE TABLE "plotconfig"( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , visible boolean); CREATE TABLE plotconfig_new( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , after integer, before integer);
-CREATE TABLE "sensor"(id TEXT,type TEXT,active NUM, metric text, path text);
+CREATE TABLE passwd (uid integer, username string, password string);
+CREATE TABLE plotconfig ( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , visible boolean); CREATE TABLE plotconfig_new( name TEXT, plotgroup TEXT, timespan TEXT, size INT, prio INT , after integer, before integer);
+CREATE TABLE sensor (id TEXT,type TEXT,active NUM, metric text, path text);
 
